2fbd54222495a0b0cf3b497cbb3de53d4075084c
Juanixter <j27790605@correo.unimet.edu.ve>
2023-10-22 17:22:41 -0400

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:clothex_app/dominio/route_generator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+//firebase imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MaterialApp(
       title: 'Clothex App',
