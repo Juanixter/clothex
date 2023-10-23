8f066a8c181cfe5290d50d97b921736848d53b35
Isabella Carrillo <isabella.carrillo@correo.unimet.edu.ve>
2023-10-22 23:10:52 -0400

    dominio

--- lib/dominio/route_generator.dart
+++ lib/dominio/route_generator.dart
@@ -1,5 +1,8 @@
 import 'package:clothex_app/aplicacion/screens/home_screen.dart';
 import 'package:clothex_app/aplicacion/screens/clothe_type.dart';
+import 'package:clothex_app/aplicacion/screens/pattern_screen.dart';
+import 'package:clothex_app/aplicacion/screens/select_material_screen.dart';
+import 'package:clothex_app/aplicacion/screens/select_pattern_screen.dart';
 import 'package:flutter/material.dart';
 
 class RouteGenerator {
@@ -13,6 +16,13 @@ class RouteGenerator {
       case '/select_clothe_type':
         return MaterialPageRoute(
             builder: (context) => const ClotheTypeScreen());
+      case '/select_material_screen':
+        return MaterialPageRoute(builder: (context) => const MaterialScreen());
+      case '/select_pattern_screen':
+        return MaterialPageRoute(builder: (context) => const PatternScreen());
+
+      case '/_pattern_screen':
+        return MaterialPageRoute(builder: (context) => SelectPatternScreen());
 
       default:
         return MaterialPageRoute(builder: (context) => const ErrorScreen());
