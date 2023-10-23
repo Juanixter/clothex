9b9997daa426175412df2e7eec1b12829f29868e
Juanixter <j27790605@correo.unimet.edu.ve>
2023-10-23 08:30:59 -0400

    listo funcionalidad

--- lib/aplicacion/screens/clothe_type.dart
+++ lib/aplicacion/screens/clothe_type.dart
@@ -9,32 +9,103 @@ class ClotheTypeScreen extends StatefulWidget {
 }
 
 class _ClotheTypeScreenState extends State<ClotheTypeScreen> {
+  int currentIndex = 0;
+
+  void changeType(int collectionIndex) {
+    setState(() {
+      currentIndex = collectionIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData().copyWith(
-          color: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData().copyWith(
+            color: Colors.black,
+          ),
         ),
-      ),
-      body: FutureBuilder(
-        future: getClotheTypes(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                return Text(snapshot.data?[index]['nombre']);
-              },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+        body: FutureBuilder(
+          future: getClotheTypes(),
+          builder: (context, snapshot) {
+            //Lista para saber los indices de cada elemento
+            List<String> indexList = [];
+
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  SingleChildScrollView(
+                    child: Row(
+                      children: [
+                        ...snapshot.data!.map((tipo) {
+                          //Añadiendo cada tipo en orden
+                          indexList.add(tipo['nombre']);
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: OutlinedButton(
+                              onPressed: () {
+                                int index = indexList.indexOf(tipo['nombre']);
+                                changeType(index);
+                              },
+                              child: Text(tipo['nombre']),
+                            ),
+                          );
+                        }).toList(),
+                      ],
+                    ),
+                  ),
+                  FutureBuilder(
+                    future: getClotheSubtypes(currentIndex),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: (context, index) {
+                            return ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.white,
+                                padding: const EdgeInsets.all(10),
+                              ),
+                              onPressed: () {},
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 150,
+                                    height: 150,
+                                    child: Image.network(
+                                      snapshot.data?[index]['imagen'],
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    snapshot.data?[index]['nombre'],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return const Text('');
+                      }
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 }
--- lib/infraestructura/firebase_service.dart
+++ lib/infraestructura/firebase_service.dart
@@ -9,9 +9,27 @@ Future<List> getClotheTypes() async {
 
   QuerySnapshot queryTypes = await collectionReferenceClotheTypes.get();
 
-  queryTypes.docs.forEach((element) {
+  for (var element in queryTypes.docs) {
     types.add(element.data());
-  });
+  }
 
   return types;
+}
+
+Future<List> getClotheSubtypes(int docIndex) async {
+  List subtypes = [];
+  String num = (docIndex + 1).toString();
+  String documento = 'Tipo$num';
+  CollectionReference mainCollectionRef = db.collection('TiposDePrenda');
+
+  CollectionReference subCollectionRef =
+      mainCollectionRef.doc(documento).collection('Subtipos');
+
+  QuerySnapshot querySubtypes = await subCollectionRef.get();
+
+  for (var element in querySubtypes.docs) {
+    subtypes.add(element.data());
+  }
+
+  return subtypes;
 }
