968d93f085476ff9a8e64b3afa39513c9afdfe19
darioaldana <dario.aldana@correo.unimet.edu.ve>
2023-11-05 11:51:54 -0400

    Aprox del formato

--- lib/aplicacion/screens/text.dart
+++ lib/aplicacion/screens/text.dart
@@ -23,7 +23,7 @@ class TextPage extends StatefulWidget {
 class _MyHomePageState extends State<TextPage> {
   final List<bool> _selectedPartes = <bool>[true, false, false];
   String seleccion = "Mangas";
-  String textito = "";
+  String textoTorso = "";
   String textoEspalda = "";
   String textoMangas = "";
   TextEditingController myController = TextEditingController();
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<TextPage> {
       print("Seleccion:   " + seleccion);
       if (seleccion == "Torso") {
         print("chest");
-        textito = s;
+        textoTorso = s;
       } else if (seleccion == "Espalda") {
         print("back");
         textoEspalda = s;
@@ -49,12 +49,12 @@ class _MyHomePageState extends State<TextPage> {
 
   void moving(String seleccion) {
     setState(() {
-      String s = textoEspalda + textito + textoMangas;
-      textito = "";
+      String s = textoEspalda + textoTorso + textoMangas;
+      textoTorso = "";
       textoEspalda = "";
       textoMangas = "";
       if (seleccion == "Torso") {
-        textito = s;
+        textoTorso = s;
       } else if (seleccion == "Espalda") {
         textoEspalda = s;
       } else {
@@ -73,12 +73,76 @@ class _MyHomePageState extends State<TextPage> {
   Widget build(BuildContext context) {
     return Column(children: [
       Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 25),
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
         child: Text(
           'TEEEEST',
           style: const TextStyle()
-              .copyWith(fontSize: 18, fontWeight: FontWeight.bold),
+              .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
         ),
+      ),
+      ToggleButtons(
+        direction: Axis.horizontal,
+        onPressed: (int index) {
+          setState(() {
+            for (int i = 0; i < _selectedPartes.length; i++) {
+              _selectedPartes[i] = i == index;
+              if (_selectedPartes[i]) {
+                seleccion = partes[i].toString().split('"')[1];
+                print(seleccion);
+                //"Mangas", "Pecho", "Espalda"
+              }
+            }
+            moving(seleccion);
+          });
+        },
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        selectedBorderColor: Colors.green[700],
+        selectedColor: Colors.white,
+        fillColor: Colors.green[200],
+        color: Colors.green[400],
+        constraints: const BoxConstraints(
+          minHeight: 30.0,
+          minWidth: 80.0,
+        ),
+        isSelected: _selectedPartes,
+        children: partes,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+        child: SizedBox(
+          width: 250,
+          height: 60,
+          child: TextField(
+            onSubmitted: (value) {
+              print(value);
+              textinho(value, seleccion);
+            },
+            controller: myController,
+            obscureText: false,
+            maxLength: 8,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Texto',
+            ),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 8,
+        width: 200,
+        child: Slider(
+            value: _currentSliderValue,
+            inactiveColor: Colors.grey,
+            activeColor: Colors.green,
+            min: 12,
+            max: 22,
+            divisions: 10,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            }),
       ),
     ]);
   }
