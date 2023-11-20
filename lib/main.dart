112b065dddc3b8af190bf257b3f730c362e8f36e
Juanixter <j27790605@correo.unimet.edu.ve>
2023-11-20 19:28:33 -0400

    nose

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:clothex_app/dominio/route_generator.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -26,8 +23,8 @@ void main() async {
       theme: ThemeData().copyWith(
         useMaterial3: true,
       ),
-      initialRoute: '/designer_screen',
+      initialRoute: '/home_screen',
       onGenerateRoute: RouteGenerator.generateRoute,
     ),
   );
-} 
+}
